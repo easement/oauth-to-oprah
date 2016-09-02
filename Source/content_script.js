7fcbb9c0914242b6b50e10242fce5310c4072d6a
Matt Billings <mattbillings@gmail.com>
2016-09-02 09:47:35 -0400

    fixing the walk method

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,35 +1,27 @@
 walk(document.body);
 
-function walk(node) 
-{
+function walk(node) {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
 	var child, next;
 	
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea') {
-		return;
-	}
-
-	switch ( node.nodeType )  
-	{
+	switch (node.nodeType) {
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
-			{
+			while (child) {
 				next = child.nextSibling;
 				walk(child);
 				child = next;
 			}
 			break;
-
 		case 3: // Text node
 			handleText(node);
 			break;
 	}
 }
+
 
 // This is where the magic happens
 function handleText(textNode) 
