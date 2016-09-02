b85e93ab6902ce703626a4e4514b4fd1cae85681
Matt Billings <mattbillings@gmail.com>
2016-09-02 09:19:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+OAuth to Oprah
+=============
+
+Chrome extension that replaces occurrences of 'OAuth' with 'Oprah'
+
+[Direct download of crx file](https://github.com/easement/oauth-to-oprah/blob/master/CloudToButt.crx?raw=true)
+
+
+Installation
+------------
+
+In Chrome, choose Window > Extensions.  Drag CloudToButt.crx into the page that appears.
+
+
+Thanks
+------
+Thanks to @panicsteve for doing all the hard work with his cloud-to-butt suff.
+https://github.com/panicsteve/cloud-to-butt
+
---
+++ Source/content_script.js
@@ -0,0 +1,49 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+	
+	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+	    || node.classList.indexOf('ace_editor') > -1) {
+		return;
+	}
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+// This is where the magic happens
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bOAuth\b/g, "Oprah");
+	v = v.replace(/\bOAuth\b/g, "Oprah");
+	v = v.replace(/\bOAuth\b/g, "Oprah");
+	v = v.replace(/\bOAuth\b/g, "Oprah");
+	v = v.replace(/\bOAuth\b/g, "Oprah");
+	
+	textNode.nodeValue = v;
+}
+
+
